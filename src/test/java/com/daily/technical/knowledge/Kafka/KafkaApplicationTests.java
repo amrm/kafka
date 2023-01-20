2a291b9f77e533aec8379fdc602fc4fb987670cc
amr <eng.amr.abdeldayem@gmail.com>
2023-01-20 17:44:54 +0300

    Initalizze Kafka Project

---
+++ src/test/java/com/daily/technical/knowledge/Kafka/KafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daily.technical.knowledge.Kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
